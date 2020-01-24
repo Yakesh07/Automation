edf08da8e29b436df464d0812e8e12ec805a0b70
YakeshPC <rakhesh.stp@gmail.com>
2020-01-24 16:52:32 +0530

    First

--- DemoPro/src/JavaPro/Hello.java
+++ DemoPro/src/JavaPro/Hello.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args) {
 
-		String name;
+		int num;
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Number");
 		num=sc.nextInt();
