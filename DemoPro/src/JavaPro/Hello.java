3a721315aefd20974bccfe2d1a159ce20b5ff77d
YakeshPC <rakhesh.stp@gmail.com>
2020-01-24 16:42:55 +0530

    First Commit

---
+++ DemoPro/src/JavaPro/Hello.java
@@ -0,0 +1,32 @@
+package JavaPro;
+
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args) {
+
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		num=sc.nextInt();
+		
+		switch (num) {
+		case 10:
+			System.out.println("Number 10 is available");
+			break;
+			
+		case 20:
+			System.out.println("Number 20 is available");
+			break;
+			
+		case 30:
+			System.out.println("Number 30 is available");
+			break;
+
+		default:
+			System.out.println("Invalid");
+			break;
+		}
+
+	}
+}
